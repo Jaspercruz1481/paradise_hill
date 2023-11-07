9042ab11e1dac2192ea3f8dd96d00f747f5a1374
jasper <jucruz@exist.com>
2023-11-07 23:08:26 +0800

    init commit

---
+++ src/main/java/com/paradisehill/agriculturecore/AgricultureCoreApplication.java
@@ -0,0 +1,13 @@
+package com.paradisehill.agriculturecore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgricultureCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgricultureCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/paradisehill/agriculturecore/AgricultureCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paradisehill.agriculturecore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgricultureCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
